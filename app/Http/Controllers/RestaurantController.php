2dc6c4595572615e203b98b84934dc56ec426c5c
mohamedAttefi <attefimohamed0@gmail.com>
2026-02-05 15:22:48 +0100

    add crud functionalities

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Restaurant;
+use App\Models\Restaurateur;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Menu;
 use Illuminate\Http\Request;
 
@@ -13,7 +15,16 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        $restaurants = Restaurant::all()->where('is_deleted', false);
+        $search = request('q');
+
+        $restaurants = Restaurant::where('is_deleted', false)
+            ->where(function ($query) use ($search) {
+                $query->where('city', 'like', "%{$search}%")
+                    ->orWhere('name', 'like', "%{$search}%")
+                    ->orWhere('cuisine_type', 'like', "%{$search}%");
+            })
+            ->get();
+
         return view('Restaurants', ['restaurants' => $restaurants]);
     }
 
@@ -22,7 +33,7 @@ class RestaurantController extends Controller
      */
     public function create()
     {
-        //
+        return view('AddRestaurant');
     }
 
     /**
@@ -30,7 +41,41 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'        => 'required|string|max:255',
+            'cuisine_type'     => 'required|string|max:100',
+            'description'     => 'nullable|string|max:255',
+            'city'        => 'required|string|max:100',
+            'address'     => 'required|string',
+            'capacity'    => 'required|integer|min:1',
+            'images.*'    => 'nullable|image|mimes:jpg,jpeg,png|max:10240',
+        ]);
+
+        // dd($request->opening_time);
+
+        $restaurateur = Auth::user()->asRestaurateur();
+
+        $restaurant = $restaurateur->restaurants()->create(
+            collect($request)->except('images')->toArray()
+        );
+
+
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $image) {
+
+                $path = $image->store('restaurants', 'public');
+
+                $restaurant->photos()->create([
+                    'filename' => $image->getClientOriginalName(),
+                    'url'      => $path,
+                    'type'     => 'gallery',
+                ]);
+            }
+        }
+
+        return redirect()
+            ->route('restaurateur.dashboard')
+            ->with('success', 'Restaurant added successfully');
     }
 
     /**
@@ -39,31 +84,65 @@ class RestaurantController extends Controller
     public function show($id)
     {
         $restaurant = Restaurant::find($id);
-        $menuItems = Menu::all()->where('restaurant_id', $id);
+        $menuItems = $restaurant->menus;
         return view('restaurant', ['restaurant' => $restaurant, 'menuItems' => $menuItems]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Restaurant $restaurant)
+    public function edit($id)
     {
-        //
+        $restaurant = Restaurant::find($id);
+        return view('EditRestaurant', ['restaurant' => $restaurant]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Restaurant $restaurant)
+    public function update(Request $request, $id)
     {
-        //
+
+        $restaurant = Restaurateur::find(Auth::id())->restaurants()->find($id);
+        $request->validate([
+            'name'          => 'required|string|max:255',
+            'cuisine_type'  => 'required|string|max:100',
+            'description'   => 'nullable|string|max:255',
+            'city'          => 'required|string|max:100',
+            'address'       => 'required|string',
+            'capacity'      => 'required|integer|min:1',
+            'images.*' => 'nullable|string|url|max:2048',
+        ]);
+
+        $restaurant->update(collect($request)->except('images')->toArray());
+
+        if ($request->images) {
+            foreach ($request->images as $image) {
+
+                $restaurant->photos()->create([
+                    'url'      => $image,
+                    'type'     => 'gallery',
+                ]);
+            }
+        }
+
+        return redirect()
+            ->route('restaurateur.dashboard')
+            ->with('success', 'Restaurant updated successfully!');
     }
+
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Restaurant $restaurant)
+    public function destroy($id)
     {
-        //
+        $restaurant = Restaurant::find($id);
+        $restaurant->update([
+            'is_deleted' => true,
+        ]);
+
+        return redirect()->back();
     }
 }
