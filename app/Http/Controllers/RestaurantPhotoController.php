f2dd0e642121f35012f31d9f6d6283aaf2fcdc8f
mohamedAttefi <attefimohamed0@gmail.com>
2026-02-05 15:23:20 +0100

    add restaurant photo controller

--- app/Http/Controllers/RestaurantPhotoController.php
+++ app/Http/Controllers/RestaurantPhotoController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Restaurateur;
+use Illuminate\Support\Facades\Auth;
+
 
 class RestaurantPhotoController extends Controller
 {
-    //
+
+    public function create()
+    {
+        return view('AddRestaurant');
+    }
 }
